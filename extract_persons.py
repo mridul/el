7f2f8eeb9e12bc4c9531c1e85ab86aa74f3759a2
mridul <mridulkapoor@gmail.com>
2016-11-11 07:44:16 +0000

    update extract persons

--- extract_persons.py
+++ extract_persons.py
@@ -3,6 +3,9 @@ import ipdb
 
 INSTANCE_TYPES_TRANSITIVE_PATH = './data/instance_types_transitive_en.ttl'
 PERSON_TYPE_PATH = './data/distinct_person_types.txt'
+OUTPUT_PERSONS_PATH = './data/distinct_persons.txt'
+OUTPUT_INSTANCE_TYPE_PATH = './data/filtered_instance_types.txt'
+DEBUG_STATUS_EVERY=10**6
 
 
 def get_person_types():
@@ -16,22 +19,36 @@ def get_person_types():
 
 def get_instance_types(type_filter_set=set()):
     instance_type_list = []
+    instance_set = set()
     with open(INSTANCE_TYPES_TRANSITIVE_PATH) as f:
+        i = 0
         for line in f:
+            i+=1
+            if i%DEBUG_STATUS_EVERY == 0:
+                print('{} lines processed'.format(i))
+
             split_line = line.strip().split(' ')
             instance = split_line[0]
-            instance_type = split_line[1]
+            instance_type = split_line[2]
             if instance_type in type_filter_set:
+                instance_set.add(instance)
                 instance_type_list.append(
                     (instance, instance_type)
                 )
 
-    return instance_type_list
+    return instance_type_list, instance_set
+
+
+def write_iterable_to_file(iterable_obj, filepath):
+    with open(filepath, 'w') as f:
+        for item in iterable_obj:
+            f.write('{}\n'.format(item))
 
 
 if __name__ == '__main__':
     types_person = get_person_types()
-    instance_type_list = get_instance_types(types_person)
-    output = [' '.join(entry) for entry in instance_type_list]
-    with open('temp.txt', 'w') as f:
-        f.writelines(output)
+    instance_type_list, instance_set = get_instance_types(types_person)
+    instance_type_list_delimited = [' '.join(entry) for entry in instance_type_list]
+
+    write_iterable_to_file(instance_set, OUTPUT_PERSONS_PATH)
+    write_iterable_to_file(instance_type_list_delimited, OUTPUT_INSTANCE_TYPE_PATH)
