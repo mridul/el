c49127e8e377226c8f8ff73d30f810c02d6d32fc
mridul <mridulkapoor@gmail.com>
2016-12-09 16:52:49 +0000

    update mention extraction - fix ngram computation and add some text normalization

--- mention_extraction.py
+++ mention_extraction.py
@@ -1,4 +1,16 @@
-import nltk
+from nltk.util import ngrams
+from nltk import word_tokenize
+
+import string
+
+import unicodedata
+import sys
+
+
+def remove_punctuation(text):
+    tbl = dict.fromkeys(i for i in range(sys.maxunicode)
+                    if unicodedata.category(chr(i)).startswith('P'))
+    return text.translate(tbl)
 
 
 def get_index():
@@ -7,20 +19,39 @@ def get_index():
     return set(text.split('\n'))
 
 def get_ngrams(text):
-    ng = set()
-    for f in [nltk.unigrams, nltk.bigrams, nltk.trigrams]:
-        ng.union(set(f(text)))
+    tokens = word_tokenize(text)
+    bigrams = ngrams(tokens, 2)
+    trigrams = ngrams(tokens, 3)
+    ng = []
+    ng.extend(tokens)
+    for n in (2, 3):
+        ng.extend([' '.join(t) for t in ngrams(tokens, n)])
+
     return ng
+
+
+def _search_in_index(tokens, index):
+    results = set()
+    for t in tokens:
+        if t in index:
+            results.add(t)
+
+    return results
 
 
 def find_all_occurences():
     index = get_index()
-    import ipdb; ipdb.set_trace()
+
     with open('test.txt') as f:
         text = f.read()
 
-    ngrams = get_ngrams(text)
+    text.replace('\n', ' \n ')
+    text = text.lower()
+    text = remove_punctuation(text)
+    tokens = get_ngrams(text)
+    matches = _search_in_index(tokens, index)
 
+    print('matches: ', matches)
 
 
 
